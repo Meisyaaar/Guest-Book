2de0b7579fc4dfaaf619c68da267dfae41547341
jstmey02 <meisyarivanty@gmail.com>
2024-10-09 13:04:45 +0700

    init

--- resources/views/admin/edit_pegawai.blade.php
+++ resources/views/admin/edit_pegawai.blade.php
@@ -14,7 +14,7 @@
     @endif
     <div class="sidebar-menu">
         <ul class="menu">
-            <li class="sidebar-title">Menu updated</li>
+            <li class="sidebar-title">Menu updated budi</li>
     
     
             <li class="sidebar-item active ">
